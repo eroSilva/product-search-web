8f4148ed89e6afac886115adb98568f4cf52127a
Erô Silva <eronildo.carneiro@grupoboticario.com.br>
2024-08-10 14:27:05 -0300

    feat: add api models

--- api/models/index.ts
+++ api/models/index.ts
@@ -1,1 +1,30 @@
-// TODO: create models
+export interface Price {
+  currency: string
+  amount: number
+  decimals: number
+}
+
+export interface Item {
+  id: string
+  title: string
+  price: Price
+  picture_url: string
+  condition: string
+  free_shipping: boolean
+}
+
+export interface ItemDetailed extends Item {
+  sold_qty: number
+  description: string
+}
+
+export interface ItemsResponse {
+  query: string
+  categories: string[]
+  items: Item[]
+}
+
+export interface ItemResponse {
+  categories: string[]
+  item: ItemDetailed
+}
