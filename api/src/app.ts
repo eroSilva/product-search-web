997be91aa21c88fd0943f063f77297e92c1b6f51
Erô Silva <eronildo.carneiro@grupoboticario.com.br>
2024-08-12 17:51:10 -0300

    feat: add cors config

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,15 @@
 import express, { Application } from 'express'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import { itemsRouter, itemRouter } from './application/controllers'
 
 const app: Application = express()
 const port = process.env.PORT || 8000
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
