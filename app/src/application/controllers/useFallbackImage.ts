26ceb2ef3e404dcee841d0a611dc701727ba103a
Erô Silva <eronildo.carneiro@grupoboticario.com.br>
2024-08-14 11:12:25 -0300

    refactor: add useCallback

--- app/src/application/controllers/useFallbackImage.ts
+++ app/src/application/controllers/useFallbackImage.ts
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 export const useFallbackImage = (src?: string) => {
   const FALLBACK_IMAGE_SRC = '/no-image-180.jpg'
   const [imgSrc, setImgSrc] = useState(src ?? FALLBACK_IMAGE_SRC)
-  const handleImageFallback = () => setImgSrc(FALLBACK_IMAGE_SRC)
+
+  const handleImageFallback = useCallback(
+    () => setImgSrc(FALLBACK_IMAGE_SRC),
+    [],
+  )
 
   return { imgSrc, handleImageFallback }
 }
