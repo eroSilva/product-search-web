70305fbc9cd319856974ff01e9699c69d882a42b
Erô Silva <eronildo.carneiro@grupoboticario.com.br>
2024-08-14 21:25:48 -0300

    refactor: improve some styles

--- app/src/presentation/components/Table/Table.tsx
+++ app/src/presentation/components/Table/Table.tsx
@@ -14,16 +14,16 @@ export const Table = ({ heading, data }: TableProps) => {
   return (
     <table className={styles.table}>
       <thead>
-        {heading?.map(({ name, value }) => (
-          <tr>
+        {heading?.map(({ name, value }, index) => (
+          <tr key={`tr-th-${index}`}>
             <th>{name ?? '-'}</th>
             <th>{value ?? '-'}</th>
           </tr>
         ))}
       </thead>
       <tbody>
-        {data?.map(({ name, value }) => (
-          <tr>
+        {data?.map(({ name, value }, index) => (
+          <tr key={`tr-td-${index}`}>
             <td>{name ?? '-'}</td>
             <td>{value ?? '-'}</td>
           </tr>
