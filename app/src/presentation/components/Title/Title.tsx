3206954fb4c87831ddbda3c1c0f8b587e694ca8d
Erô Silva <eronildo.carneiro@grupoboticario.com.br>
2024-08-14 21:47:05 -0300

    refactor: remove unused component

--- app/src/presentation/components/Title/Title.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './title.module.scss'
-
-interface TitleProps {
-  children: React.ReactNode
-}
-
-export const Title = ({ children }: TitleProps) => {
-  return <h1 className={styles.title}>{children}</h1>
-}
--- app/src/presentation/components/Title/__tests__/Title.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Title } from '@/presentation/components/Title'
-
-describe('Title', () => {
-  it('should render the title', () => {
-    render(<Title>Hello World</Title>)
-    expect(screen.getByText('Hello World')).toBeInTheDocument()
-  })
-})
