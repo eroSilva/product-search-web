bdbb141ccce2ad3b01bc1d015b975a354d57b5ea
Erô Silva <eronildo.carneiro@grupoboticario.com.br>
2024-08-14 21:38:55 -0300

    feat: add request caches

--- app/src/application/services/itemService.ts
+++ app/src/application/services/itemService.ts
@@ -1,7 +1,9 @@
 import { ItemEntity, ItemsEntity } from '@product-search-web/shared'
 
 export const getItem = async (id: string): Promise<ItemEntity | Response> => {
-  const res = await fetch(`${process.env.API_ENDPOINT}/item/${id}`)
+  const res = await fetch(`${process.env.API_ENDPOINT}/item/${id}`, {
+    next: { revalidate: 1800 },
+  })
 
   return res.ok ? res.json() : res
 }
@@ -9,7 +11,9 @@ export const getItem = async (id: string): Promise<ItemEntity | Response> => {
 export const getItems = async (
   term: string,
 ): Promise<ItemsEntity | Response> => {
-  const res = await fetch(`${process.env.API_ENDPOINT}/items?search=${term}`)
+  const res = await fetch(`${process.env.API_ENDPOINT}/items?search=${term}`, {
+    next: { revalidate: 1800 },
+  })
 
   return res.ok ? res.json() : res
 }
